487b3c8d7c53183eaab5a4b79a6ce224afd868ea
cptran777 <cptran14@gmail.com>
2016-06-23 12:24:26 -0700

    Add ability to server for title

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -13,11 +13,29 @@ var Songs = Backbone.Collection.extend({
         _.each(data.results, function(song) {
           that.add(song);
         });
-        
       },
       error: function() {
         console.log('YOU FAIL SIR');
       }
     });
+  },
+
+  search: function(value) {
+    var that = this;
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      type: 'GET',
+      data: 'where={"title":{"$regex":"(?i)' + value + '"}}',
+      success: function(data) {
+        that.reset();
+        _.each(data.results, function(song) {
+          that.add(song);
+        });
+      },
+      error: function() {
+        console.log('YOU FAIL SIR');
+      }
+    });    
   }
+
 });
--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -15,12 +15,26 @@ var LibraryView = Backbone.View.extend({
     }, this);
   },
 
+  events: {
+    click: function(e) {
+      if (e.target.className === 'search-button') {
+        console.log('searching for ' + $('.search-value').val());
+        this.collection.search($('.search-value').val());
+      }
+    }
+  },
+
   render: function() {
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
     // see http://api.jquery.com/detach/
     this.$el.children().detach();
 
     this.$el.html('<th>Library</th>').append(
+      '<br><div><form>Title:<input type="text" name"titleSearch" class="search-value">' + 
+      '<input type="button" value="search" class="search-button"></form></div>'
+      );
+
+    this.$el.append(
       this.collection.map(function(song) {
         return new LibraryEntryView({model: song}).render();
       })
