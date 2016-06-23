6bbc73ddc756f06c1c0aa5aff5ae6ffa901d4c7f
cptran777 <cptran14@gmail.com>
2016-06-22 22:12:29 -0700

    Progress on adding and removing from the queue

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,17 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td class="queue-entry"><%= title %>'),
+
+  events: {
+
+  },
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,34 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+
+    this.collection.on('add', function() {
+      this.render();
+    }, this);
+
+    this.collection.on('dequeue', function() {
+      this.render();
+    }, this);
+  },
+
+  events: {
   },
 
   render: function() {
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
 
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song) {
+        console.log('adding to the queue');
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
